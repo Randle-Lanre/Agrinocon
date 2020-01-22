7ecbef77dbec898ae4ea51645e3b977d7f0536b0
Randle Kehinde Olanrewaju <kennyrandle09@hotmail.co.uk>
2020-01-22 14:58:10 +0000

    updated crud functionality of the weather controller

--- AgrinoconWebApi/Controllers/Api/WeatherController.cs
+++ AgrinoconWebApi/Controllers/Api/WeatherController.cs
@@ -6,6 +6,9 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Script.Serialization;
+using AgrinoconWebApi.Dtos;
+using AgrinoconWebApi.Models;
+using AutoMapper;
 using Microsoft.Ajax.Utilities;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -14,20 +17,27 @@ namespace AgrinoconWebApi.Controllers.Api
 {
     public class WeatherController : ApiController
     {
+        private ApplicationDbContext _context;
 
+        public WeatherController()
+        {
+            _context = new ApplicationDbContext();
+        }
 
         static HttpClient http = new HttpClient();
+
+
         //Get api/weather
-        //TODO: add paramenter for user to supply optional location infomation or use default
+
         [Route("api/getcurrentwether")]
         public async Task<IHttpActionResult> GetCurrentWeather(String city = "Dublin", String country = "Ireland")
         {
             var getApiKey = Environment.GetEnvironmentVariable("WEATHERAPIKEY");
-            
+
             var fetchWeatherInformation =
                 $"https://api.weatherbit.io/v2.0/current?city={city}&country={country}&key={getApiKey}";
 
-            
+
 
             var response = await http.GetAsync(fetchWeatherInformation);
 
@@ -43,30 +53,64 @@ namespace AgrinoconWebApi.Controllers.Api
 
         //TODO:add authorization
         [HttpPost]
-        public IHttpActionResult CreateLocationPreference()
+        public IHttpActionResult CreateLocationPreference(WeatherInformationDto weatherInformationDto)
         {
-            string stringtest = "posted";
 
-            return Ok(stringtest);
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var weather = Mapper.Map<WeatherInformationDto, WeatherInformation>(weatherInformationDto);
+            _context.WeatherInformations.Add(weather);
+            _context.SaveChanges();
+
+
+            weatherInformationDto.Id = weather.Id;
+
+
+            return Created(new Uri(Request.RequestUri + "/" + weather.Id), weatherInformationDto);
+
+            //string stringtest = "posted";
+            //return Ok(stringtest);
             //return Created(new Uri(Request.RequestUri + "/" +), )
         }
 
         //TODO:add authorization
+        //PUT api/weather/1
         [HttpPut]
-        public IHttpActionResult UpdateWeather()
+        public IHttpActionResult UpdateWeather(int id, WeatherInformationDto weatherInformationDto)
         {
-            string stringtest = "Put successful";
 
-            return Ok(stringtest);
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var weatherInformationInDb = _context.WeatherInformations.SingleOrDefault(w => w.Id == id);
+
+            if (weatherInformationInDb == null)
+                return NotFound();
+            Mapper.Map(weatherInformationDto, weatherInformationInDb);
+
+            _context.SaveChanges();
+            return Ok();
+
+            //string stringtest = "Put successful";
+
+            //return Ok(stringtest);
         }
 
         //TODO:add authorization with specified roles
+        //DELETE  api/weather/1
         [HttpDelete]
-        public IHttpActionResult DeleteWeather()
+        public IHttpActionResult DeleteWeather(int id)
         {
+            var weatherInformationInDb = _context.WeatherInformations.SingleOrDefault(w => w.Id == id);
 
-            string stringtest = "sucessfully deleted";
-            return Ok(stringtest);
+            if (weatherInformationInDb == null)
+                return NotFound();
+            _context.WeatherInformations.Remove(weatherInformationInDb);
+            _context.SaveChanges();
+            return Ok();
+            //string stringtest = "sucessfully deleted";
+            //return Ok(stringtest);
         }
 
 
